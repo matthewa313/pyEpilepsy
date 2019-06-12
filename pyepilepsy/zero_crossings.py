17fb536e60caf959bf4c840a689ea6045629f726
Matthew Anderson <mwanderson04@gmail.com>
2019-06-11 21:23:05 -0600

    Update zero_crossings.py

--- pyepilepsy/zero_crossings.py
+++ pyepilepsy/zero_crossings.py
@@ -1,4 +1,13 @@
 import numpy as np
 
-def zero_crossings(sig):
-  return None
+def zero_crossings(sig, lengthArray):
+  '''
+  lengthArray = range(len(sig))
+  This variable was added to minimize RAM space.
+  '''
+  numCrossings = 0
+  for i in lengthArray:
+    if (sig[i] > 0 and sig[i+1] <=0 ) or (sig[i] < 0 and sig[i+1] >=0 ):
+      numCrossings += 1
+  
+  return numCrossings
