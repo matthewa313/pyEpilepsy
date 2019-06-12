00de580d10411d86f94342858ccd6f90e6e491f9
Matthew Anderson <mwanderson04@gmail.com>
2019-06-11 19:12:09 -0600

    Update mean.py

--- pyepilepsy/mean.py
+++ pyepilepsy/mean.py
@@ -1,4 +1,5 @@
 from numpy.core import umath
+from numpy import float128
 
 def mean(a, axis=1):
     axis = (axis,)
@@ -6,5 +7,5 @@ def mean(a, axis=1):
     for ax in axis:
         items *= a.shape[ax]
     rcount = items
-    ret = umath.add.reduce(a, axis, dtype, None, False)
+    ret = umath.add.reduce(a, axis, float128, None, False)
     return ret / rcount
