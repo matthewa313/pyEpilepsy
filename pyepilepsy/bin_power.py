6fa71cf3f9d5296399c2de33360af4a994280d20
Matthew Anderson <mwanderson04@gmail.com>
2019-06-14 16:32:33 -0600

    Update bin_power.py

--- pyepilepsy/bin_power.py
+++ pyepilepsy/bin_power.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-[0.5,4,7,12,30], 256
-
 def bin_power(sig):
     """
     This is NOT a general purpose function. Compute power
@@ -24,20 +22,16 @@ def bin_power(sig):
     power
         list
         spectral power in each frequency bin.
-    power_ratio
-        list
-        spectral power in each frequency bin normalized by total power in ALL
-        frequency bins.
     """
 
     c = abs ( np.fft.fft(sig) )
-    power = np.array([0,0,0,0])
-    for freq_index in [0,1,2,3]:
+    band = [0.5,4,7,12,30,70,180]
+    power = np.array([0,0,0,0,0,0])
+    for freq_index in [0,1,2,3,4,5]:
         freq = float(band[freq_index])
         next_freq = float(band[freq_index + 1])
         power[freq_index] = sum(
             c[int(np.floor(freq / Fs * len(sig))): 
                 int(np.floor(next_freq / Fs * len(sig)))]
         )
-    power_ratio = power / sum(power)
-    return power, power_ratio
+    return power
